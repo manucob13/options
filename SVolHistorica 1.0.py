798e64ab8ae52dd30f96e484aee92b040e9d8eb9
Manuel Izquierdo <mizq14@gmail.com>
2025-04-12 18:53:46 +1000

    Update SVolHistorica 1.0.py

--- SVolHistorica 1.0.py
+++ SVolHistorica 1.0.py
@@ -51,11 +51,11 @@ monthly_vol = monthly_vol[[1,2,3,4,5,6,7,8,9,10,11,12,'Anual']]
 monthly_vol.columns = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 
                        'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic', 'Anual']
 
-# Crear tabla HTML estilizada con fondo blanco
+# Crear tabla HTML estilizada con texto blanco y fondo oscuro
 styled_table = (
     monthly_vol.style
         .format("{:.2%}")
-        .set_table_attributes('style="width:100%;border-collapse:collapse;font-size:14px; background-color: white;"')
+        .set_table_attributes('style="width:100%;border-collapse:collapse;font-size:14px; background-color: #333; color: white;"')
         .to_html()
 )
 
